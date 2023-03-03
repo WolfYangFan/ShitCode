7e6e0299c9acf5abf6d54d7786d65497a257cb82
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2023-03-03 21:48:48 +0800

    64112

--- LAnalytics/index.js
+++ LAnalytics/index.js
@@ -1,45 +1,53 @@
-!(function () {
+!(function() {
     'use strict';
     var endpoint = window.document.currentScript.getAttribute('data-api') ||
-        new URL(window.document.currentScript.src).origin + '/_api/collect';
-        
+      new URL(window.document.currentScript.src)
+      .origin + '/_api/collect';
+  
     function warn(reason) {
-        console.warn('Ignoring Event: ' + reason);
+      console.warn('Ignoring Event: ' + reason);
     }
-
+  
     function trigger(eventName, options) {
-        if (/^localhost$|^127(\.[0-9]+){0,2}\.[0-9]+$|^\[::1?\]$/.test(window.location.hostname) || 'file:' === window.location.protocol) return warn('localhost or file');
-        if (
-            !window._phantom ||
-            !window.__nightmare ||
-            !window.navigator.webdriver ||
-            !window.Cypress
-        ) {
-            var data = {};
-            data.d = window.document.currentScript.getAttribute('data-domain') // Domain
-            data.e = eventName // Event name
-            data.ua = navigator.userAgent || null // User-Agent
-            data.l = window.location.href // Location
-            data.rd = new URL(window.document.referrer).host || null //Referrer domain
-            if (options && options.meta) {
-                payload.m = JSON.stringify(options.meta)
-            }
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', endpoint, true)
-            xhr.setRequestHeader('Content-Type', 'text/plain')
-            xhr.send(JSON.stringify(data))
-            request.onreadystatechange = function() {
-                if (request.readyState === 4) {
-                  options && options.callback && options.callback()
-                }
-              }
+      if (/^localhost$|^127(\.[0-9]+){0,2}\.[0-9]+$|^\[::1?\]$/.test(window.location.hostname) || 'file:' === window.location.protocol) return warn('localhost or file');
+      if (
+        !window._phantom ||
+        !window.__nightmare ||
+        !window.navigator.webdriver ||
+        !window.Cypress
+      ) {
+        var data = {};
+        data.d = window.document.currentScript.getAttribute('data-domain') // Domain
+        data.e = eventName // Event name
+        data.ua = navigator.userAgent || null // User-Agent
+        data.l = window.location.href // Location
+        data.rd = new URL(window.document.referrer)
+          .host || null //Referrer domain
+        if (options && options.meta) {
+          payload.m = JSON.stringify(options.meta)
         }
-    }
-    window._la.push = trigger
-    function page() {
-        lastPage = location.pathname
-        trigger('pageview')
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', endpoint, true)
+        xhr.setRequestHeader('Content-Type', 'text/plain')
+        xhr.send(JSON.stringify(data))
+        request.onreadystatechange = function() {
+          if (request.readyState === 4) {
+            options && options.callback && options.callback()
+          }
+        }
       }
-})();
-// Test Env. Usage: 
-// <script defer data-api="https://hc-ping.com/3855e1be-d3da-4af3-8cdf-ce95e8178a38" data-domain="my.domain.com" src="https://xiaozhu2007.github.io/ShitCode/LAnalytics/index.js"></script>
+    }
+    var queue = (window._la && window._la.q) || []
+    window._la = trigger
+    for (var i = 0; i < queue.length; i++) {
+      trigger.apply(this, queue[i])
+    }
+  
+    function page() {
+      lastPage = location.pathname
+      trigger('pageview')
+    }
+    page();
+  })();
+  // Test Env. Usage: 
+  // <script defer data-api="https://hc-ping.com/3855e1be-d3da-4af3-8cdf-ce95e8178a38" data-domain="my.domain.com" src="https://xiaozhu2007.github.io/ShitCode/LAnalytics/index.js"></script>
